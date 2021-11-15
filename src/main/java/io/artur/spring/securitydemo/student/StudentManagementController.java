6792553f0e6b25d5a94bbe702fceba55f78fbcec
Artur Przychodzen <artur2p@gmail.com>
2021-11-15 16:39:55 +0100

    secure endpoints using pre authorize annotations

--- src/main/java/io/artur/spring/securitydemo/student/StudentManagementController.java
+++ src/main/java/io/artur/spring/securitydemo/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package io.artur.spring.securitydemo.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -19,21 +20,25 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMIN_TRAINEE')")
     public List<Student> getStudents() {
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println("Registering student: " +student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Long studentId) {
         System.out.println("Deleting student with ID: " + studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable Long studentId, @RequestBody Student student) {
         System.out.println("Updating student with ID: " + studentId + " Student: " + student);
     }
